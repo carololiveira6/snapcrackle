a3518d1d9115d2b944762385140b0906078d9990
Carolina Oliveira <carolinaoliveira@id.uff.br>
2021-02-10 19:26:57 -0300

    Criada a funçao prime

--- snapcrackle.js
+++ snapcrackle.js
@@ -1,8 +1,7 @@
 function snapCrackle(maxValue) {
     let str = ''
     for (let i = 1; i <= maxValue; i++) {
-        debugger
-        if (i % 2 !== 0 && i % 5 === 0) { 
+        if (i % 2 !== 0 && i % 5 === 0) {
             str = str + 'SnapCrackle, '
         } else if (i % 2 !== 0) {
             str = str + 'Snap, '
@@ -15,4 +14,53 @@ function snapCrackle(maxValue) {
     return str
 }
 
-console.log(snapCrackle(12))
+function primo(x) {
+    if (x < 2) {
+        return false
+    } else if (x > 2 && x % 2 === 0) {
+        return false
+    } else if (x !== 3 && x % 3 === 0) {
+        return false
+    } else if (x !== 5 && x % 5 === 0) {
+        return false
+    } else if (x !== 7 && x % 7 === 0) {
+        return false
+    }
+    return true
+}
+
+function snapCracklePrime(maxValue) {
+    debugger
+    let str = ''
+    for (let i = 1; i <= maxValue; i++) {
+        if (i % 2 === 0) {
+            if (primo(i)) {
+                str = str + 'Prime, '
+            } else if (i % 5 === 0) {
+                str = str + 'Crackle, '
+            } else {
+                str = str + i.toString() + ', '
+            }
+        } else {
+            if (primo(i)) {
+                if (i % 5 === 0) {
+                   str = str + 'SnapCracklePrime, ' 
+                } else {
+                    str = str + ' SnapPrime, '
+                }
+            } else {
+                if (i % 5 === 0){
+                    str = str + 'SnapCrackle, '
+                }else {
+                    str = str + 'Snap, '
+                }
+            }
+        }
+    }
+    return str
+}
+
+// console.log(snapCrackle(12))
+console.log(snapCracklePrime(15))
+// Snap, Prime, SnapPrime, 4, SnapCracklePrime, 6, SnapPrime, 8, Snap, Crackle, SnapPrime, 12, SnapPrime, 14, SnapCrackle,
+
